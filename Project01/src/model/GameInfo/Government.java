b9e0238ba86a306ea64888dd7499c51c3e359d25
alkazm086 <119755139+alkazm086@users.noreply.github.com>
2023-04-25 23:58:23 +0330

    Add files via upload
    Committed by Alireza Kazemi moghaddam

--- Project01/src/model/GameInfo/Government.java
+++ Project01/src/model/GameInfo/Government.java
@@ -5,7 +5,7 @@ import model.User;
 import java.util.HashMap;
 
 public class Government {
-    private Integer stone;
+
     private Integer unemployed;
     private User owner;
     private Integer popularity=0;
@@ -13,20 +13,18 @@ public class Government {
     private Integer coin=0;
     private Integer taxRate=0;
     private Integer fearRate=0;
-    private HashMap<String,Integer> food=new HashMap();
-    {
-        food.put("Apple",0);
-        food.put("Meat",0);
-        food.put("Cheese",0);
-        food.put("Bread",0);
-    }
+    private Integer population=0;
+//    private HashMap<String,Integer> food=new HashMap();
+//    {
+//        food.put("Apple",0);
+//        food.put("Meat",0);
+//        food.put("Cheese",0);
+//        food.put("Bread",0);
+//    }
     public Integer getPopularity() {
         return popularity;
     }
 
-    public HashMap<String, Integer> getHashMap() {
-        return food;
-    }
 
     public void setOwner(User owner) {
         this.owner = owner;
@@ -61,14 +59,6 @@ public class Government {
     }
 
     public void setFoodRate(Integer rate){}
-
-    public void setStone(Integer stone) {
-        this.stone = stone;
-    }
-
-    public Integer getStone() {
-        return stone;
-    }
 
     private void setPopularity(Integer popularity) {
         this.popularity = popularity;
